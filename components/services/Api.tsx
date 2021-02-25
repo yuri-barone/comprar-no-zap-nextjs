437f7ec19bff27a6ce738d1ab9f1f1efb1b2fbf1
yuri-barone <yuri-barone@hotmail.com>
2021-02-25 15:09:32 -0300

    arrumando a build

--- components/services/Api.tsx
+++ components/services/Api.tsx
@@ -1,21 +1,22 @@
-import axios from 'axios';
-import { setupCache } from 'axios-cache-adapter';
-import addResponseErrorMidleware from './responseErrorMidleware';
+import axios from "axios";
+import { setupCache } from "axios-cache-adapter";
+import addResponseErrorMidleware from "./responseErrorMidleware";
 
 const MINUTOS = 2;
 
-axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common["Content-Type"] = "application/json";
 const cache = setupCache({ maxAge: MINUTOS * 60 * 1000 });
 
-const create = (baseURL: string | undefined) => axios.create({
-  baseURL,
-  timeout: 50000,
-  adapter: cache.adapter,
-});
+const create = (baseURL: string | undefined) =>
+  axios.create({
+    baseURL,
+    timeout: 50000,
+    adapter: cache.adapter,
+  });
 
-export const mainApi = create('http://localhost:3030');
+export const mainApi = create("https://app.comprarnozap.com");
 addResponseErrorMidleware(mainApi);
 
-export const configureMainApi = (token:string) => {
-  mainApi.defaults.headers.common.Authorization = 'Bearer '.concat(token);
+export const configureMainApi = (token: string) => {
+  mainApi.defaults.headers.common.Authorization = "Bearer ".concat(token);
 };
