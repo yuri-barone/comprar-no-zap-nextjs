77e863813d70d1240104ff339ea5bf8f6cce5e29
yuri-barone <yuri-barone@hotmail.com>
2021-03-18 11:39:45 -0300

    tentando ativar google pixel

--- pages/_document.js
+++ pages/_document.js
@@ -19,23 +19,25 @@ const googleTag = () => ({
   `,
 });
 
-const facebookPixel = `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '824748165051647');
-fbq('track', 'PageView');`;
+const facebookPixel = () => ({
+  __html: `!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '824748165051647');
+  fbq('track', 'PageView');`,
+});
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <script src={facebookPixel} />
+          <script dangerouslySetInnerHTML={facebookPixel()} />
           <noscript>
             <img
               height="1"
